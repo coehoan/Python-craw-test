a3eeab260db7bbccdf6fcc42329d654f82da5030
coehoan <coehoan@naver.com>
2022-05-11 17:53:51 +0900

    네이버 뉴스 크롤링 후 스프링으로 출력

---
+++ src/main/java/site/metacoding/newstest/domain/Naver.java
@@ -0,0 +1,23 @@
+package site.metacoding.newstest.domain;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Document(collection = "navers") // greendb/navers
+public class Naver {
+    @Id
+    private String _id;
+    private String company;
+    private String title;
+    private String createdAt;
+}
